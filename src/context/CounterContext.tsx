81d493f6019c27b449e2730b96758e4d044596fb
de-scientist <gitaumark502@gmail.com>
2025-11-17 11:03:35 +0300

    export the CounterContext

--- src/context/CounterContext.tsx
+++ src/context/CounterContext.tsx
@@ -1,1 +1,12 @@
 import { createContext } from "react";
+
+interface CounterContext {
+    onIncrement: () => void;
+    onDecrement: () => void;
+    onIncrementWith10: () => void;
+    onDecrementWith10: () => void;
+}
+
+const CounterContext = createContext<CounterContext>();
+
+export default CounterContext;
